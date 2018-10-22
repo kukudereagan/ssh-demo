537398e7c719c280548a47b34494965ccf42e2ac
kukudereagan <kukudereagan@163.com>
2018-10-22 10:20:18 +0800

    add

--- src/main/java/com/lg/test/ctrl/Shell.java
+++ src/main/java/com/lg/test/ctrl/Shell.java
@@ -5,6 +5,9 @@ import com.jcraft.jsch.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Shell {
     //远程主机的ip地址
@@ -98,8 +101,37 @@ public class Shell {
        // shell.execute("su - oracle <<! ./u01/app/ogg/ggsci <<! info all ");
         shell.execute("./../u01/app/ogg/test.sh");
         ArrayList<String> stdout = shell.getStandardOutput();
+        List list = new ArrayList<>();
         for (String str : stdout) {
-            System.out.println(str);
+            if(str.startsWith("EXTRACT")){
+                System.out.println(str);
+                String  EXTRACT = str.substring(str.indexOf("EXTRACT")+"EXTRACT".length(),str.indexOf("Last Started"));
+                String  LastStarted = str.substring(str.indexOf("Last Started")+"Last Started".length(),str.indexOf("Status"));
+                String  Status = str.substring(str.indexOf("Status")+"Status".length(),str.length());
+                Map map = new HashMap();
+                map.put("EXTRACT",EXTRACT.replaceAll(" ",""));
+                map.put("LastStarted",LastStarted.replaceAll(" ",""));
+                map.put("Status",Status.replaceAll(" ",""));
+                list.add(map);
+            }
         }
+
+        System.out.println(list.toString());
+
+
+        /**
+         * EXTRACT    EXTA
+         * Last Started 2018-06-20 15:37
+         * Status ABENDED
+         * Checkpoint Lag       00:00:00 (updated 2683:12:09 ago)
+         * Log Read Checkpoint  Oracle Redo Logs                     2018-06-29 16:29:00
+         * Seqno 2670,
+         * RBA 10435584
+         * SCN 0.55572924 (55572924)
+         *
+         * EXTRACT    EXTB
+         * Last Started 2018-06-20 17:13
+         * Status ABENDEDCheckpoint Lag       00:00:00 (updated 2683:12:04 ago)Log Read Checkpoint  Oracle Redo Logs                     2018-06-29 16:29:05  Seqno 2670, RBA 10466816                     SCN 0.55572968 (55572968)EXTRACT    EXTC      Last Started 2018-05-24 10:10   Status STOPPEDCheckpoint Lag       00:00:00 (updated 2900:04:10 ago)Log Read Checkpoint  Oracle Redo Logs                     2018-06-20 15:36:57  Seqno 2557, RBA 32289280                     SCN 0.52397557 (52397557)EXTRACT    EXTKAFKA  Last Started 2018-09-04 17:44   Status ABENDEDCheckpoint Lag       00:00:00 (updated 572:11:08 ago)Log Read Checkpoint  Oracle Redo Logs                     2018-09-25 15:30:00  Seqno 3550, RBA 13998080                     SCN 0.82205776 (82205776)
+         */
     }
 }
